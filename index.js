476137a749a504d9dd687f411a41d0c76a3b1f04
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-04-01 16:04:09 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Find the element with the specified ID
+    const textElement = document.getElementById('text');
+  
+    textElement.textContent = 'This is really cool!';
+  });
